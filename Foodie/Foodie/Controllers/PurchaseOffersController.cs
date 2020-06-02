e0f471d6ce2255d64f16a610e83e518dc8d371d4
julia850722 <julia850722@gmail.com>
2020-06-02 16:43:16 +0800

    2020/6/2
    新增代買賣方功能與買方頁面

---
+++ Foodie/Foodie/Controllers/PurchaseOffersController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Foodie.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Foodie.Controllers
+{
+    [Authorize]
+    public class PurchaseOffersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PurchaseOffers
+        public ActionResult Index()
+        {
+            var Id = User.Identity.GetUserId();
+            if (Id == null)
+            {
+                return PartialView(db.PurchaseOffers.OrderByDescending(p => p.Seller.Id).ToList());
+                //return View(db.PurchaseOffers.ToList());
+            }
+            else
+            {
+                var username = User.Identity.Name;
+                var user = db.Users.Where(p => p.UserName == username).FirstOrDefault();
+                return PartialView(db.PurchaseOffers.Where(p => p.Seller.Id == user.Id).ToList());
+            }
+        }
+
+        public ActionResult RequestIndex()
+        {
+            var RestaurantList = from r in db.PurchaseOffers select r;
+            ViewBag.RestaurantLists = new SelectList(RestaurantList, "OfferedRestaurant", "OfferedRestaurant");
+            var Id = User.Identity.GetUserId();
+            if (Id == null)
+            {
+                return View(db.PurchaseOffers.OrderByDescending(p => p.Seller.Id).ToList());
+                //return View(db.PurchaseOffers.ToList());
+            }
+            else
+            {
+                var username = User.Identity.Name;
+                var user = db.Users.Where(p => p.UserName == username).FirstOrDefault();
+                return View(db.PurchaseOffers.Where(p => p.Seller.Id == user.Id).ToList());
+            }
+        }
+
+        public ActionResult Info()
+        {
+            return View(db.PurchaseOffers.OrderByDescending(p => p.Seller.Id).ToList());
+        }
+        // GET: PurchaseOffers/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOffer purchaseOffer = db.PurchaseOffers.Find(id);
+            if (purchaseOffer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOffer);
+        }
+
+        // GET: PurchaseOffers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PurchaseOffers/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DeadLineTime,OfferedRestaurant,OfferTime")] PurchaseOffer purchaseOffer)
+        {
+            if (ModelState.IsValid)
+            {
+                string sellername = User.Identity.Name;
+                var sellerid = User.Identity.GetUserId(); 
+                purchaseOffer.Id = Guid.NewGuid();
+                purchaseOffer.DeadLineTime = DateTime.Now.AddMinutes(purchaseOffer.OfferTime);
+                purchaseOffer.Seller = db.Users.Where(p => p.Id == sellerid).FirstOrDefault();
+                purchaseOffer.SellerName = db.Users.Where(p => p.Id == sellerid).FirstOrDefault().UserName;
+                db.PurchaseOffers.Add(purchaseOffer);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+
+            return View(purchaseOffer);
+        }
+
+        // GET: PurchaseOffers/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOffer purchaseOffer = db.PurchaseOffers.Find(id);
+            if (purchaseOffer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOffer);
+        }
+
+        // POST: PurchaseOffers/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DeadLineTime,OfferedRestaurant")] PurchaseOffer purchaseOffer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchaseOffer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+            return View(purchaseOffer);
+        }
+
+        // GET: PurchaseOffers/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PurchaseOffer purchaseOffer = db.PurchaseOffers.Find(id);
+            if (purchaseOffer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchaseOffer);
+        }
+
+        // POST: PurchaseOffers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            PurchaseOffer purchaseOffer = db.PurchaseOffers.Find(id);
+            db.PurchaseOffers.Remove(purchaseOffer);
+            db.SaveChanges();
+            return RedirectToAction("Create");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Foodie/Foodie/Models/PuchaseRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Models
+{
+    public class PuchaseRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public virtual ApplicationUser Buyer { get; set; }
+
+        [Required]
+        [Display(Name ="欲申請代買餐廳")]
+        public string RestaurantName { get; set; }
+    }
+    
+}
