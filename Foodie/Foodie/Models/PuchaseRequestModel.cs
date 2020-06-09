3bfe7f00a6de0abc0f399ef4b6da7246d73b2afa
julia850722 <julia850722@gmail.com>
2020-06-09 13:48:28 +0800

    master+julia
    20200609子檸已完成offer和request功能
    和master采汝的專案合併在一起

---
+++ Foodie/Foodie/Models/PuchaseRequestModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Models
+{
+    public class PuchaseRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid PurchaseOffersId { get; set; }
+
+        public string Rname { get; set; }
+
+        public virtual ApplicationUser Seller { get; set; }
+
+        public virtual ApplicationUser Buyer { get; set; }
+
+        public DateTime? SubmittedTime { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+    
+}
---
+++ Foodie/Foodie/Models/PurchaseOfferModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Foodie.Models
+{
+    public class PurchaseOffer
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("代買外送員")]
+        public string SellerName { get; set; }
+        public virtual ApplicationUser Seller { get; set; }
+       
+        [Display(Name = "代買結束時間")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:g}", ApplyFormatInEditMode = true)]
+        public DateTime? DeadLineTime { get; set; }
+      
+        [Required]
+
+        [DisplayName("預計幾分鐘後結單")]
+        public int OfferTime { get; set; }
+        [DisplayName("餐廳地點")]
+        public string OfferedRestaurant { get; set; }
+    }
+}
